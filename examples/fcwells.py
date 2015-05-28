ef4e59f76058e17fe66d89fc7efe6e2bd19c138f
Andrew Hah <andrew.hah@kcc.com>
2015-05-28 20:52:13 +0000

    add timer

--- examples/fcwells.py
+++ examples/fcwells.py
@@ -4,6 +4,18 @@ import changepoint.bootstrap as btstrp
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+
+
+def timed(f):
+
+    def _f(*args, **kwargs):
+        start = time.time()
+        res = f(*args, **kwargs)
+        end = time.time()
+        print end - start
+
+    return _f
 
 
 def load_data():
@@ -13,21 +25,23 @@ def load_data():
     return np.array([float(f.strip()) for f in lines])[1500:2500]
 
 
+@timed
 def binseg_example():
     xs = load_data() 
     cps = map(lambda x:x[0], btstrp.find_all_cps(xs, crit_val=0.01))
-    plt.plot(np.arange(len(xs)), xs)
-    for cp in cps:
-        plt.axvline(x=cp, c="grey", lw=2.0)
-    plt.show()
+    #plt.plot(np.arange(len(xs)), xs)
+    #for cp in cps:
+    #    plt.axvline(x=cp, c="grey", lw=2.0)
+    #plt.show()
 
 
+@timed
 def bayes_example():
     xs = load_data()
     R, M, V = bayes.find_all_cps(xs, cp_prob=1./250, plot=False)
     mu_hat = np.sum(R*M, axis=0)
     var_hat = np.sum(R*V, axis=0)
-    bayes.plot_results(xs, mu_hat, var_hat)
+    #bayes.plot_results(xs, mu_hat, var_hat)
 
 
 if __name__ == "__main__":
